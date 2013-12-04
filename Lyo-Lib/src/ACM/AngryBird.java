d687a3e7a9b3507ce20456a3be3ac069838fa754
lyozhou <lyozhou@gmail.com>
2013-12-04 13:36:03 +0800

    acm

--- Lyo-Lib/src/ACM/AngryBird.java
+++ Lyo-Lib/src/ACM/AngryBird.java
@@ -18,16 +18,21 @@ public class AngryBird {
 		Scanner cin = new Scanner(new BufferedInputStream(System.in));
 		int caseNum = cin.nextInt();
 		for(int i=0;i<caseNum;i++){
-			int result = 0;
-			String flyMile = cin.next();
+			int result = 1;
+			int basePoint = 0;
+			int flyMile = Integer.parseInt(cin.next());
 			int pigNum = Integer.parseInt(cin.next());
+			int[] pigPos = new int[pigNum];
 			for(int k=0;k<pigNum;k++){
-				int pigPos = Integer.parseInt(cin.next());
-				if(Integer.parseInt(flyMile) < pigPos){
+				pigPos[k] = Integer.parseInt(cin.next());
+			}
+			for(int k=1;k<pigPos.length;k++){
+				if(pigPos[k]- pigPos[basePoint]>flyMile){
+					basePoint = k;
 					result++;
 				}
 			}
-			System.out.println("Case "+i+1+": "+result);
+			System.out.println("Case "+ (i+1)+": "+result);
 		}
 	}
 }
